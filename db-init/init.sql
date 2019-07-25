0d89909196271b7ac18e672d5a13d59b675725e1
diogofalken <diogofalken0@gmail.com>
2019-07-25 14:57:24 +0100

    Some Fixes

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,1 +1,3 @@
 CREATE TABLE IF NOT EXISTS users (id int NOT NULL AUTO_INCREMENT, name TEXT, birthDate DATE, city TEXT, PRIMARY KEY(id));
+SET GLOBAL max_connections=50000;
+SET GLOBAL thread_cache_size=250;
